c84a1871424f010dca36c931448466c568f700fa
Gharieb Khalifa <blacksnow.soon@gmail.com>
2026-05-27 01:45:29 +0300

    feat: implement custom Cairo font and modernize desk UI with CSS variable-driven theme styles

--- go_frappe_themez/hooks.py
+++ go_frappe_themez/hooks.py
@@ -25,7 +25,7 @@ app_license = "mit"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/go_frappe_themez/css/go_frappe_themez.css"
+app_include_css = "/assets/go_frappe_themez/css/desk_theme.css"
 # app_include_js = "/assets/go_frappe_themez/js/go_frappe_themez.js"
 
 # include js, css files in header of web template
